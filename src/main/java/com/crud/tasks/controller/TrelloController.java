0b53d31071bab30be71a3327316d2305b4ec08de
Sboolasty <sebastianwojdal@wp.pl>
2023-05-23 20:31:04 +0200

    22.1

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/trello")
+
+@CrossOrigin("*")
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    public TrelloController(TrelloClient trelloClient) {
+        this.trelloClient = trelloClient;
+    }
+
+    @GetMapping("boards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.forEach(trelloBoardDto -> {
+            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+        });
+    }
+}
---
+++ src/main/java/com/crud/tasks/domain/TrelloBoardDto.java
@@ -0,0 +1,32 @@
+package com.crud.tasks.domain;
+
+import lombok.Data;
+
+
+
+public class TrelloBoardDto {
+    private String name;
+    private String id;
+
+    public TrelloBoardDto(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+    public TrelloBoardDto() {
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
